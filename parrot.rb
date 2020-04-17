86163248821d8d1ab90bad4028a02ff94673210e
Catherine Price <catprice444@gmail.com>
2020-04-17 18:06:53 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase='Squawk!')
-  p (phrase)
-end
+# def parrot(phrase='Squawk!')
+#   p (phrase)
+# end
 
-parrot
+def parrot (phrase = "Squawk!")
+  chirp = phrase
+  p (phrase)
+  chirp
+end
